0ce4c1ed3b10d4f06eade8facb7cd611b65a999c
SallaParkkila <62949251+SallaParkkila@users.noreply.github.com>
2020-09-06 13:36:05 +0300

    Create main.cpp

---
+++ t10/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int strcount(char s,char c)
+{
+    int count=0;
+    while ((s)!='\0')
+    {
+        if ((*s)==c)
+        count++;
+        s++;
+    }
+    return count;
+}
+int main()
+{
+    char s[]="olio-ohjelmointi";
+    char c;
+    cout << "mita merkkia haetaan?" << endl;
+    cin.get(c);
+    cout << "merkkeja loytyi " << strcount(s, c) << endl;
+    return 0;
+}
